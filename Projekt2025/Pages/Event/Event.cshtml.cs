1361e5f586ed941dea6a43bf462855a8b5d6efac
Ahmed Alshghri <aha004@edu.zealand.dk>
2025-05-13 02:23:02 +0200

    ..

--- Projekt2025/Pages/Event/Event.cshtml.cs
+++ Projekt2025/Pages/Event/Event.cshtml.cs
@@ -5,6 +5,14 @@ namespace Projekt2025.Pages.Event
 {
     public class EventModel : PageModel
     {
+        private readonly Projekt2025.Models.fgonline_dk_db_zooContext _context;.
+
+        public EventModel(Projekt2025.Models.fgonline_dk_db_zooContext context)
+        {
+            _context = context;
+        }
+
+        public List<Event> Event { get; set; } = default!;
         public void OnGet()
         {
         }
