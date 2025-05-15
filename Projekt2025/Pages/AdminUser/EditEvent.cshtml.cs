608010b8f3e3f17adfcf302c7ba4a33c213e8374
frederikG1 <kendamaftw123@gmail.com>
2025-05-15 12:13:14 +0200

    oprettet Create, Edit og Delete til admindashboard og det virker som det skal - ser også ud til at opdatere i databasen

---
+++ Projekt2025/Pages/AdminUser/EditEvent.cshtml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Projekt2025.Models;
+
+namespace Projekt2025.Pages.AdminUser
+{
+    public class EditEventModel : PageModel
+    {
+        private readonly Projekt2025.Models.fgonline_dk_db_zooContext _context;
+
+        public EditEventModel(Projekt2025.Models.fgonline_dk_db_zooContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Event Event { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventEntity = await _context.Events.FirstOrDefaultAsync(m => m.EventId == id);
+            if (eventEntity == null)
+            {
+                return NotFound();
+            }
+            Event = eventEntity;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Event).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventExists(Event.EventId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("AdminDashboard");
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Events.Any(e => e.EventId == id);
+        }
+    }
+}
