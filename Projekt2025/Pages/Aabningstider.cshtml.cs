16892f6780d18a1906307c90be861a8947a1d84c
frederikG1 <kendamaftw123@gmail.com>
2025-05-21 11:11:03 +0200

    ændrede lige åbningstiderne :D

--- Projekt2025/Pages/Aabningstider.cshtml.cs
+++ Projekt2025/Pages/Aabningstider.cshtml.cs
@@ -11,22 +11,22 @@ namespace Projekt2025.Pages
         {
             AdresseHtml = @"
                 <h2>Roskilde</h2>
-                <p>Maglegårdsvej 2 2B</p>
+                <p>MaglegÃ¥rdsvej 2 2B</p>
                 <p>4000 Roskilde</p>
                 <p>tlf 12 34 56 78</p>
-                <p><a href='https://www.google.com/maps/search/?api=1&query=Maglegårdsvej+2+4000+Roskilde' target='_blank'>Find vej</a></p>
+                <p><a href='https://www.google.com/maps/search/?api=1&query=Maglegï¿½rdsvej+2+4000+Roskilde' target='_blank'>Find vej</a></p>
             ";
 
             AabningstiderHtml = @"
-                <h3>Åbningstider</h3>
+                <h3>Ã…bningstider</h3>
                 <table style='border-spacing: 0;'>
-                    <tr><td>Mandag:</td><td style='padding-left: 10px;'>Lukket</td></tr>
-                    <tr><td>Tirsdag:</td><td style='padding-left: 10px;'>Lukket</td></tr>
-                    <tr><td>Onsdag:</td><td style='padding-left: 10px;'>Lukket</td></tr>
-                    <tr><td>Torsdag:</td><td style='padding-left: 10px;'>Lukket</td></tr>
-                    <tr><td>Fredag:</td><td style='padding-left: 10px;'>14-17</td></tr>
-                    <tr><td>Lørdag:</td><td style='padding-left: 10px;'>Lukket</td></tr>
-                    <tr><td>Søndag:</td><td style='padding-left: 10px;'>Lukket</td></tr>
+                    <tr><td>Mandag:</td><td style='padding-left: 10px;'>12:00-16:00</td></tr>
+                    <tr><td>Tirsdag:</td><td style='padding-left: 10px;'>12:00-16:00</td></tr>
+                    <tr><td>Onsdag:</td><td style='padding-left: 10px;'>12:00-16:00</td></tr>
+                    <tr><td>Torsdag:</td><td style='padding-left: 10px;'>12:00-16:00</td></tr>
+                    <tr><td>Fredag:</td><td style='padding-left: 10px;'>14:00-22:00</td></tr>
+                    <tr><td>LÃ¸rdag:</td><td style='padding-left: 10px;'>14:00-22:00</td></tr>
+                    <tr><td>SÃ¸ndag:</td><td style='padding-left: 10px;'>12:00-16:00</td></tr>
                 </table>
             ";
         }
